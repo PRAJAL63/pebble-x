d964562614276f043981cd1523ac510c839baec5
PRAJAL63 <grgprajal01@gmail.com>
2025-11-17 00:39:32 +0545

    adding animation or illustration on register for better ui

--- my-react-app/src/pages/Register.jsx
+++ my-react-app/src/pages/Register.jsx
@@ -16,11 +16,11 @@ export default function Register() {
   return (
     <div className="flex min-h-screen">
       {/* Left Image/Illustration */}
-      <div className="hidden lg:flex w-1/2 bg-linear-to-tr from-green-800 to-teal-400 items-center justify-center">
+      <div className="hidden lg:flex w-1/2 bg-linear-to-tr from-green-800 to-teal-800 items-center justify-center">
         <img
           src="https://images.unsplash.com/photo-1581091215362-32f0f2286ca0?auto=format&fit=crop&w=800&q=80"
           alt="Hardware wala image"
-          className="w-4/5 rounded-2xl shadow-lg"
+          className="w-4/5 rounded-2xl shadow-lg text-white"
         />
       </div>
 
